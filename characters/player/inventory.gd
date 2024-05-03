6fb87d80fefb80f3587b0fb2bfffab093f4cbcab
ThePraet0rian <thePraet0rian404@gmail.com>
2024-05-03 09:09:34 +0200

    Changes

--- characters/player/inventory.gd
+++ characters/player/inventory.gd
@@ -1,11 +1,9 @@
 extends CanvasLayer
 
 
-
 func _ready() -> void:
 	
 	pass
-
 
 
 func _input(event: InputEvent) -> void:
@@ -13,4 +11,4 @@ func _input(event: InputEvent) -> void:
 	if event.is_action_pressed("shift"):
 		
 		get_tree().paused = false
-		#queue_free()
+		queue_free()
