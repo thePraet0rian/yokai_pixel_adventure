cd46efeaac154eb453d5346b5439fb125e3bda1c
thePraet0rian <thePraet0rian404@gmail.com>
2024-06-15 22:31:55 +0200

    Changes

--- src/test/test.gd
+++ src/test/test.gd
@@ -1,20 +1,2 @@
 class_name test extends Node2D
 
-
-
-func _ready() -> void:
-	
-	var a = test.new()
-	a.stest()
-
-
-func ftest() -> void:
-	
-	print("wat")
-
-
-class t extends test: 
-	
-	func stest() -> void:
-		super.ftest()
-	
